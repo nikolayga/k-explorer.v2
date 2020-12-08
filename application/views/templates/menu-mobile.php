9866eba45056d68a205a5e993487cad12a329b13
nikolayga <71999088+nikolayga@users.noreply.github.com>
2020-12-08 10:33:49 +0600

    Update menu-mobile.php

--- application/views/templates/menu-mobile.php
+++ application/views/templates/menu-mobile.php
@@ -2,7 +2,11 @@
 	<nav class="nav d-flex justify-content-between">
 	  <a class="p-2 <?=empty($this->uri->segment(1))?'active':''?>" href="/"><span data-feather="grid"></span> Dashboard</a>
 	  <a class="p-2 <?=$this->uri->segment(1)=="deposits"?'active':''?>" href="/deposits/"><span data-feather="credit-card"></span> Deposits</a>
+	  <a class="p-2 <?=$this->uri->segment(1)=="operators"?'active':''?>" href="/operators/"><span data-feather="users"></span> Operators</a>
+	  <a class="p-2 <?=$this->uri->segment(1)=="grants"?'active':''?>" href="/grants/"><span data-feather="award"></span> Grants</a>
+	  <a class="p-2 <?=$this->uri->segment(1)=="keeptransfers"?'active':''?>" href="/keeptransfers/">KEEP Transfers</a>
+	  <a class="p-2 <?=$this->uri->segment(1)=="transfers"?'active':''?>" href="/transfers/">tBTC Transfers</a>
 	  <a class="p-2 <?=$this->uri->segment(1)=="tbtc_mints"?'active':''?>" href="/tbtc_mints/">tBTC mints</a>
-	  <a class="p-2 <?=$this->uri->segment(1)=="transfers"?'active':''?>" href="/transfers/">Transfers</a>
+	  
 	</nav>
 </div>
