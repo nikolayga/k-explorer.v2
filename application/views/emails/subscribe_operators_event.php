ae934c06f20d159621419ea420e618e43783a481
nikolayga <71999088+nikolayga@users.noreply.github.com>
2020-12-08 10:29:27 +0600

    Create subscribe_operators_event.php

---
+++ application/views/emails/subscribe_operators_event.php
@@ -0,0 +1,11 @@
+The operator <b><a href="https://keep-explorer.info/operators/<?=$operator?>" target="_blank"><?=$operator?></a></b> has deposit with <b>"<?=$event_name?>"</b> event<br>
+
+
+<br>
+<p>Go to the operator page - <a href="https://keep-explorer.info/operators/<?=$operator?>"><?=$operator?></a></p>
+<p>Go to the deposit page - <a href="https://keep-explorer.info/deposit/<?=$depositAddress?>"><?=$depositAddress?></a></p>
+<br><br>
+<p><small>
+<a href="https://keep-explorer.info">keep-explorer.info</a><br>
+This is an automated message, please do not reply.
+</small></p>
