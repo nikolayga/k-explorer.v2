2acb5f8bcca2e7490b899774859f12ea1ef4abfc
nikolayga <71999088+nikolayga@users.noreply.github.com>
2020-12-08 10:13:47 +0600

    Update Deposits.php

--- application/controllers/Deposits.php
+++ application/controllers/Deposits.php
@@ -8,7 +8,7 @@ class Deposits extends CI_Controller {
         $config['base_url'] = base_url().'deposits/index';
         
         $config['total_rows'] = count($this->deposits_model->get_list(true));
-        $config['per_page'] = 20;
+        $config['per_page'] = 50;
         $config['uri_segment'] = 3;
         $config['attributes'] = array('class' => 'page-link');
         $config['reuse_query_string']  = TRUE;
@@ -39,7 +39,10 @@ class Deposits extends CI_Controller {
  
 		$data = array();
         $data['items'] = $this->deposits_model->get_list(false, $config['per_page'], $offset);
-        $data['lotsizes'] = $this->deposits_model->get_lot_list();
+		$data['total'] = $this->deposits_model->get_total();
+        $data['found'] = $config['total_rows'];
+
+		$data['lotsizes'] = $this->deposits_model->get_lot_list();
         
         $this->load->view('templates/header');
         $this->load->view('deposits/index', $data);
@@ -53,7 +56,7 @@ class Deposits extends CI_Controller {
 		 
 		 if(!empty($_POST['addMembers']) && !empty($_POST['members'])) $this->deposits_model->saveKeepMembers($address, $_POST['members']);
 		 if(!empty($_POST['addBond']) && !empty($_POST['bond'])) $this->deposits_model->saveKeepBond($address, $_POST['bond']);
-		 
+		 if(!empty($_POST['updateState']) && !empty($_POST['state'])) $this->deposits_model->saveState($address, intval($_POST['state']));
 		 
 		 $this->load->view('templates/header');
          $this->load->view('deposits/view', $data);
