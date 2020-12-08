3221b700bc313f58b26c4e36ad9bcfd38aaf264a
nikolayga <71999088+nikolayga@users.noreply.github.com>
2020-12-08 10:28:46 +0600

    Create subscribe_operators_collateralization.php

---
+++ application/views/emails/subscribe_operators_collateralization.php
@@ -0,0 +1,11 @@
+The operator <b><a href="https://keep-explorer.info/operators/<?=$operator?>" target="_blank"><?=$operator?></a></b> has deposits with collateralization less than <?=$collateralization?>%
+<br><br>
+<b>Deposits:</b>
+<?=$deposits?>
+<br>
+<p>Go to the operator page - <a href="https://keep-explorer.info/operators/<?=$operator?>"><?=$operator?></a></p>
+<br><br>
+<p><small>
+<a href="https://keep-explorer.info">keep-explorer.info</a><br>
+This is an automated message, please do not reply.
+</small></p>
