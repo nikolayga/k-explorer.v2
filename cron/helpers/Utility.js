c60126770e776907e7c02c668c82e56b0aacdad6
nikolayga <71999088+nikolayga@users.noreply.github.com>
2020-12-08 09:30:38 +0600

    Update Utility.js

--- cron/helpers/Utility.js
+++ cron/helpers/Utility.js
@@ -28,5 +28,33 @@ module.exports = {
 				});
 			}
 		})	
+	},
+	
+	addSubscribeOperatorQueue(db, event){
+		db.connection.query("SELECT * FROM depositHistory WHERE keepMembers IS NOT NULL AND depositContractAddress='"+event.returnValues._depositContractAddress+"'", {}, function(err, result) {
+			if(err==null) {
+				var members = result[0].keepMembers.split(",");
+				members.forEach(function(item, index, result) {
+					db.connection.query("SELECT * FROM operatorEventsSubscribe WHERE operator='"+item+"' AND JSON_CONTAINS(events,'[\""+event.event+"\"]')", {}, function(err, result) {
+						if(err==null) {
+							result.forEach(function(item, index, result) {
+								var sbscr = {
+									'datetime':module.exports.toMysqlFormat(new Date()),
+									'address':item.address, 
+									'contractAddress': event.returnValues._depositContractAddress,
+									'operator': item.operator,
+									'email': item.email,
+									'event': event.event,
+									'sended': 0,
+								}
+
+								db.connection.query('INSERT INTO operatorSubscribeQueue SET ?', sbscr, function(err, result) {})
+						
+							});
+						}
+					})	
+				});
+			}
+		})
 	}
 };
